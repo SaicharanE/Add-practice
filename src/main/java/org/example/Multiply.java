235bbfbe7ddc547077729f03713a149dfc076f1b
saicharan <saicharan.epparapally@gmail.com>
2025-06-18 14:19:26 +0530

    Added Add.java and Multiply.java

---
+++ src/main/java/org/example/Multiply.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Multiply {
+    static int a = 9;
+    static int b = 4;
+
+    public static void main(String[] args) {
+        System.out.println(a*b);
+    }
+}
